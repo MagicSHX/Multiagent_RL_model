973bccd88270b3da201b5c8bbadf62d573f9a5b8
MagicSHX <34002636+MagicSHX@users.noreply.github.com>
2020-06-04 23:15:20 +0800

    add DL mode

--- model/DL_Network_Model.py
+++ model/DL_Network_Model.py
@@ -7,12 +7,12 @@ class Net(nn.Module):
     def __init__(self):
         #super: https://forums.fast.ai/t/why-super-init/20390/2
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
+        #self.conv1 = nn.Conv2d(3, 6, 5)
+        #self.pool = nn.MaxPool2d(2, 2)
+        #self.conv2 = nn.Conv2d(6, 16, 5)
         #https://stackoverflow.com/questions/47128044/calculating-input-and-output-size-for-conv2d-in-pytorch-for-image-classification
         
-        self.fc1 = nn.Linear(9, 27)
+        self.fc1 = nn.Linear(252, 27)
         self.dropout1 = nn.Dropout(p=0.9, inplace=False)
         self.fc2 = nn.Linear(27, 81)
         #inplace: https://stackoverflow.com/questions/58589128/what-is-the-meaning-of-in-place-in-dropout
@@ -24,9 +24,8 @@ class Net(nn.Module):
         self.fc5 = nn.Linear(18, 3)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16*5*5)
+        #x = self.pool(F.relu(self.conv1(x)))
+        #x = self.pool(F.relu(self.conv2(x)))
         x = F.relu(self.fc1(x))
         #dropout example: https://medium.com/@zhang_yang/scaling-in-neural-network-dropout-layers-with-pytorch-code-example-11436098d426
         #x = self.dropout1(x)
